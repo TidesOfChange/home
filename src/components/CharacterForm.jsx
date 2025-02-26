d619e778c9b85d1b6ce000d51bae7177d46d8bed
Azathotha <93633837+Azathotha@users.noreply.github.com>
2025-02-25 21:45:38 -0800

    rename play page to play_interface to make it less confusing

--- src/components/CharacterForm.jsx
+++ src/components/CharacterForm.jsx
@@ -296,7 +296,7 @@ const CharacterForm = () => {
         ) : (
           <button
             onClick={() => {
-              window.location.href = "/play";
+              window.location.href = "/play_interface";
             }}
             className="cursor-pointer px-4 py-2 bg-yellow-500 text-white rounded hover:bg-yellow-700"
           >
