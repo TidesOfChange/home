38690be9c6f92d85d960280a7069fa1354fc0658
kmkomori <121131653+kmkomori@users.noreply.github.com>
2025-03-12 11:15:19 -0700

    configure base URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,8 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://tidesofchange.github.io",
+  base: "/home",
   vite: {
     plugins: [tailwindcss()]
   },
