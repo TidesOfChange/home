556e28529fdb56ce412510513991de8ca1fe0ee9
Kumiko Komori <kmkomori@gmail.com>
2025-03-14 11:09:04 -0700

    configured git action and astro url

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,8 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://tidesofchange.github.io/home",
-  // base: "/home",
+  site: "https://tidesofchange.github.io",
+  base: "/home",
   vite: {
     plugins: [tailwindcss()]
   },
