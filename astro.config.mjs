35d810b214e1988d81b8cf5ab8e8a670ab6d7c0f
Spencer Coleman <spencer@swc.dev>
2025-05-01 22:12:40 -0700

    Wireframe index page; adjust routing

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://tidesofchange.github.io',
-    base: '/home',
+    base: '/',
     integrations: [react()],
     vite: {
         plugins: [tailwindcss()],
