030b4b251c43639dbea2b2e6266ee3b3dae51ecf
matt0923 <74699880+matt0923@users.noreply.github.com>
2025-05-28 15:27:47 -0700

    Added Cinzel fony to PDF export

--- src/components/character-creator/PdfExportButton.jsx
+++ src/components/character-creator/PdfExportButton.jsx
@@ -1,4 +1,5 @@
 import { jsPDF } from 'jspdf';
+import "../../styles/Cinzel-normal.js";
 
 const PdfExportButton = ({character}) => {
     const exportToPDF = () => {
@@ -7,20 +8,23 @@ const PdfExportButton = ({character}) => {
             unit: "in",
             format: [8.5, 11]
           });
+        doc.setFont("Cinzel-Regular", "normal");
+        doc.setFontSize(16);
+        console.log(doc.getFontList());
         doc.addImage(`${character.species_image}`, 2.125, 0, 6.375, 2);
         // doc.text(`Name: ${character.name}`, 2, 4); (ADD 1 TO EVERY OTHER ATTRIBUTE AFTER)
-        doc.text(`Species: ${character.species}`, 2.5, 3);
-        doc.text(`Background: ${character.background}`, 2.5, 5);
+        doc.text(`Species: ${character.species}`, 2.125, 3);
+        doc.text(`Background: ${character.background}`, 2.125, 4);
         doc.text(
             `Stats: 
+            . Knowledge: ${character.stats.Knowledge} 
+            . Intuition: ${character.stats.Intuition} 
             . Strength: ${character.stats.Strength} 
-            . Intelligence: ${character.stats.Intelligence} 
-            . Wisdom: ${character.stats.Wisdom} 
-            . Dexterity: ${character.stats.Dexterity} 
-            . Constitution: ${character.stats.Constitution} 
-            . Charisma: ${character.stats.Charisma}`,
-            2.5,
-            6
+            . Agility: ${character.stats.Agility} 
+            . Personality: ${character.stats.Personality} 
+            . Health: ${character.stats.Health}`,
+            2.125,
+            5
         );
         doc.save('character.pdf');
     };
