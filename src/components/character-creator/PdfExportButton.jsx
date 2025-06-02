d34f03a98aa2c9c1ed6d1a85ba8992a1b4e83340
matt0923 <74699880+matt0923@users.noreply.github.com>
2025-06-02 00:01:51 -0700

    small changes

--- src/components/character-creator/PdfExportButton.jsx
+++ src/components/character-creator/PdfExportButton.jsx
@@ -3,29 +3,18 @@ import "../../styles/Cinzel-normal.js";
 
 const PdfExportButton = ({character}) => {
     const exportToPDF = () => {
+        var img = new Image();
+        img.src = "/home/toc.png"
         const doc = new jsPDF({
             orientation: "landscape",
             unit: "in",
             format: [8.5, 11]
           });
-        doc.setFont("Cinzel-Regular", "normal");
-        doc.setFontSize(16);
-        console.log(doc.getFontList());
-        doc.addImage(`${character.species_image}`, 2.125, 0, 6.375, 2);
-        // doc.text(`Name: ${character.name}`, 2, 4); (ADD 1 TO EVERY OTHER ATTRIBUTE AFTER)
-        doc.text(`Species: ${character.species}`, 2.125, 3);
-        doc.text(`Background: ${character.background}`, 2.125, 4);
-        doc.text(
-            `Stats: 
-            . Knowledge: ${character.stats.Knowledge} 
-            . Intuition: ${character.stats.Intuition} 
-            . Strength: ${character.stats.Strength} 
-            . Agility: ${character.stats.Agility} 
-            . Personality: ${character.stats.Personality} 
-            . Health: ${character.stats.Health}`,
-            2.125,
-            5
-        );
+        const w = doc.internal.pageSize.getWidth();
+        const h = doc.internal.pageSize.getHeight();
+        doc.addImage(img, 'PNG', 0, 0, w, h);
+        doc.setFont("Cinzel-Regular");
+        doc.text(`Player name`, 2.125, 0, 6.375, 2);
         doc.save('character.pdf');
     };
     return (
